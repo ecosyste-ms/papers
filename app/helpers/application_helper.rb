25b13b4b9c90cc6ef2ccf4cb969c53ac9f987947
Andrew Nesbitt <andrewnez@gmail.com>
2023-11-03 15:38:25 +0000

    display some package details

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,29 @@ module ApplicationHelper
   def meta_description
     @meta_description || 'An open API service providing mapping between scientific papers and software projects that are mentioned in them.'
   end
+
+  def sort_by_semver_range(hash, limit)
+    hash.sort_by{|_k,v| -v}
+               .first(limit)
+               .sort_by{|k,_v|
+                 k.gsub(/\~|\>|\<|\^|\=|\*|\s/,'')
+                 .gsub('-','.')
+                 .split('.').map{|i| i.to_i}
+               }.reverse
+  end
+
+  def download_period(downloads_period)
+    case downloads_period
+    when "last-month"
+      "last month"
+    when "total"
+      "total"
+    end
+  end
+
+  def sanitize_user_url(url)
+    return unless url && url.is_a?(String)
+    return unless url =~ /\A#{URI::regexp}\z/
+    sanitize_url(url, :schemes => ['http', 'https'])
+  end
 end
