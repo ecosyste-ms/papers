dc51e3bb4e8d9a628cfd368e8919bf59f5889e8b
Andrew Nesbitt <andrewnez@gmail.com>
2025-08-03 17:13:25 +0100

    Add site-wide cache headers for production environment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,19 @@
 class ApplicationController < ActionController::Base
   include Pagy::Backend
 
+  before_action :set_cache_headers
+
   def default_url_options(options = {})
     Rails.env.production? ? { :protocol => "https" }.merge(options) : options
+  end
+
+  private
+
+  def set_cache_headers
+    return unless Rails.env.production?
+    
+    expires_in 2.weeks, public: true
+    response.headers['Cache-Control'] = 'public, max-age=1209600, s-maxage=1209600'
+    response.headers['Vary'] = 'Accept-Encoding'
   end
 end
